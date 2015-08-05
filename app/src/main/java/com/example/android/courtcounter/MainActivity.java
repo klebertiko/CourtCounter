ee7c8adf5389bbf62759d150586e8c614765dcd9
kleber.toyota <kleber.toyota@concretesolutions.com.br>
2015-08-05 20:35:30 -0300

    Initial commit

---
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -0,0 +1,139 @@
+package com.example.android.courtcounter;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    // Tracks the score for Team A
+    private int scoreTeamA = 0;
+    // Tracks the score for Team B
+    private int scoreTeamB = 0;
+
+    // Instance to hold the score for Team A
+    private static final String SCORE_TEAM_A_INSTANCE = "SCORE_TEAM_A";
+    // Instance to hold the score for Team B
+    private static final String SCORE_TEAM_B_INSTANCE = "SCORE_TEAM_B";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(savedInstanceState != null){
+            this.scoreTeamA = savedInstanceState.getInt(SCORE_TEAM_A_INSTANCE);
+            this.scoreTeamB = savedInstanceState.getInt(SCORE_TEAM_B_INSTANCE);
+        }
+
+        this.displayForTeamA(this.scoreTeamA);
+        this.displayForTeamB(this.scoreTeamB);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(SCORE_TEAM_A_INSTANCE, this.scoreTeamA);
+        outState.putInt(SCORE_TEAM_B_INSTANCE, this.scoreTeamB);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        /*if (id == R.id.action_settings) {
+            return true;
+        }*/
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    /**
+     * Increase the score for Team A by 1 point.
+     */
+    public void addOneForTeamA(View v) {
+        this.scoreTeamA = this.scoreTeamA + 1;
+        this.displayForTeamA(this.scoreTeamA);
+    }
+
+    /**
+     * Increase the score for Team A by 2 points.
+     */
+    public void addTwoForTeamA(View v) {
+        this.scoreTeamA = this.scoreTeamA + 2;
+        this.displayForTeamA(this.scoreTeamA);
+    }
+
+    /**
+     * Increase the score for Team A by 3 points.
+     */
+    public void addThreeForTeamA(View v) {
+        this.scoreTeamA = this.scoreTeamA + 3;
+        this.displayForTeamA(this.scoreTeamA);
+    }
+
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Increase the score for Team B by 1 point.
+     */
+    public void addOneForTeamB(View v) {
+        this.scoreTeamB = this.scoreTeamB + 1;
+        this.displayForTeamB(this.scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team B by 2 points.
+     */
+    public void addTwoForTeamB(View v) {
+        this.scoreTeamB = this.scoreTeamB + 2;
+        this.displayForTeamB(this.scoreTeamB);
+    }
+
+    /**
+     * Increase the score for Team B by 3 points.
+     */
+    public void addThreeForTeamB(View v) {
+        this.scoreTeamB = this.scoreTeamB + 3;
+        this.displayForTeamB(this.scoreTeamB);
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    /**
+     * Displays the given score for Team B.
+     */
+    public void resetScore(View v) {
+        this.scoreTeamA = 0;
+        this.scoreTeamB = 0;
+        this.displayForTeamA(this.scoreTeamA);
+        this.displayForTeamB(this.scoreTeamB);
+    }
+}
